6e8ec4662242fad6ffbefd3d78fbb978cfb4e842
CHENYANLIANG-duke <46186328+CHENYANLIANG-duke@users.noreply.github.com>
2019-01-02 18:01:15 +0800

    加入專案檔案。

---
+++ MP1407/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace MP1407
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_CloseReader = new System.Windows.Forms.Button();
+            this.btn_OpenReader = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtmsg = new System.Windows.Forms.TextBox();
+            this.btn_AutoReadUltraLight = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLOT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_CloseReader
+            // 
+            this.btn_CloseReader.Enabled = false;
+            this.btn_CloseReader.Location = new System.Drawing.Point(110, 12);
+            this.btn_CloseReader.Name = "btn_CloseReader";
+            this.btn_CloseReader.Size = new System.Drawing.Size(86, 29);
+            this.btn_CloseReader.TabIndex = 94;
+            this.btn_CloseReader.Text = "Close Reader";
+            this.btn_CloseReader.UseVisualStyleBackColor = true;
+            this.btn_CloseReader.SizeChanged += new System.EventHandler(this.btn_CloseReader_SizeChanged);
+            this.btn_CloseReader.Click += new System.EventHandler(this.btn_CloseReader_Click);
+            // 
+            // btn_OpenReader
+            // 
+            this.btn_OpenReader.Location = new System.Drawing.Point(18, 12);
+            this.btn_OpenReader.Name = "btn_OpenReader";
+            this.btn_OpenReader.Size = new System.Drawing.Size(86, 29);
+            this.btn_OpenReader.TabIndex = 96;
+            this.btn_OpenReader.Text = "Open Reader";
+            this.btn_OpenReader.UseVisualStyleBackColor = true;
+            this.btn_OpenReader.Click += new System.EventHandler(this.btn_OpenReader_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtmsg);
+            this.groupBox1.Location = new System.Drawing.Point(12, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(388, 105);
+            this.groupBox1.TabIndex = 97;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "NDEF Message";
+            // 
+            // txtmsg
+            // 
+            this.txtmsg.Location = new System.Drawing.Point(6, 21);
+            this.txtmsg.Multiline = true;
+            this.txtmsg.Name = "txtmsg";
+            this.txtmsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtmsg.Size = new System.Drawing.Size(376, 75);
+            this.txtmsg.TabIndex = 97;
+            // 
+            // btn_AutoReadUltraLight
+            // 
+            this.btn_AutoReadUltraLight.Enabled = false;
+            this.btn_AutoReadUltraLight.Location = new System.Drawing.Point(202, 11);
+            this.btn_AutoReadUltraLight.Name = "btn_AutoReadUltraLight";
+            this.btn_AutoReadUltraLight.Size = new System.Drawing.Size(120, 30);
+            this.btn_AutoReadUltraLight.TabIndex = 94;
+            this.btn_AutoReadUltraLight.Text = "AutoRead UltraLight";
+            this.btn_AutoReadUltraLight.UseVisualStyleBackColor = true;
+            this.btn_AutoReadUltraLight.Click += new System.EventHandler(this.btn_AutoReadUltraLight_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(202, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 22);
+            this.textBox1.TabIndex = 98;
+            this.textBox1.Text = "10";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 12);
+            this.label1.TabIndex = 99;
+            this.label1.Text = "Thread time :";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Unitech NFC";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 12);
+            this.label2.TabIndex = 101;
+            this.label2.Text = "UID :";
+            // 
+            // txtUID
+            // 
+            this.txtUID.Location = new System.Drawing.Point(59, 56);
+            this.txtUID.Name = "txtUID";
+            this.txtUID.Size = new System.Drawing.Size(55, 22);
+            this.txtUID.TabIndex = 100;
+            this.txtUID.Text = "14";
+            this.txtUID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.TabIndex = 103;
+            this.label3.Text = "LOT :";
+            // 
+            // txtLOT
+            // 
+            this.txtLOT.Location = new System.Drawing.Point(59, 83);
+            this.txtLOT.Name = "txtLOT";
+            this.txtLOT.Size = new System.Drawing.Size(55, 22);
+            this.txtLOT.TabIndex = 102;
+            this.txtLOT.Text = "20";
+            this.txtLOT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(165, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 12);
+            this.label4.TabIndex = 105;
+            this.label4.Text = "Url :";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(202, 56);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(55, 22);
+            this.txtUrl.TabIndex = 104;
+            this.txtUrl.Text = "16";
+            this.txtUrl.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 223);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLOT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_AutoReadUltraLight);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_CloseReader);
+            this.Controls.Add(this.btn_OpenReader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mp1407_Unitech v1.2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Button btn_CloseReader;
+        internal System.Windows.Forms.Button btn_OpenReader;
+        private System.Windows.Forms.GroupBox groupBox1;
+        internal System.Windows.Forms.Button btn_AutoReadUltraLight;
+        private System.Windows.Forms.TextBox txtmsg;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLOT;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUrl;
+    }
+}
+
---
+++ MP1407/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace MP1407
+{
+    public partial class Form1 : Form
+    {
+        int M_iResult;
+        List<byte> devcomport = new List<byte>();
+        List<byte> devcomport2 = new List<byte>();
+        string[] ports;
+        string strUID;
+        private byte[] Key = new byte[(int)RDINT.MIFARE_ONE_DEF.CARD_KEY_LEN];
+
+        Thread thread0;
+        Thread thread1;
+        Thread thread2;
+        Thread TagDataProcess;
+
+        byte[] Data;
+        byte[] Data2;
+        byte[] DataURL1;
+        byte[] DataURL2;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readconfig();
+        }
+
+        private void btn_OpenReader_Click(object sender, EventArgs e)
+        {
+            uint intBar = 0;
+            txtmsg.Clear();
+
+            for (int i = 0; i < ports.Length; i++)
+            {
+                var comindex = Convert.ToInt32(ports[i].Remove(0, 3));
+                devcomport.Add((byte)comindex);
+            }
+
+            for (int i = 0; i < devcomport.Count; i++)
+            {
+                M_iResult = (int)RDINT.RDINTsys.OpenReader(devcomport[i], uint.Parse("38400"), "00000000", RDINT.TURN_ON_OFF.TURN_ON, uint.Parse("0"), out intBar);
+                if (!(M_iResult == 0))
+                {
+                    ShowStatus("Open Reader" + (i + 1).ToString() + " 失敗。錯誤代碼:" + M_iResult);
+                }
+                else
+                {
+                    ShowStatus("Open Reader  com" + devcomport[i].ToString() + " 成功");
+                    devcomport2.Add((byte)devcomport[i]);
+                }
+            }
+
+            btn_OpenReader.Enabled = false;
+            btn_CloseReader.Enabled = true;
+            btn_AutoReadUltraLight.Enabled = true;
+
+        }
+
+        private void btn_CloseReader_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtmsg.Clear();
+
+                for (int i = 0; i < devcomport2.Count; i++)
+                {
+                    M_iResult = (int)RDINT.RDINTsys.CloseReader(devcomport2[i]);
+
+
+                    if (M_iResult == 0)
+                    {
+                        ShowStatus("Close Reader" + (i + 1).ToString() + "成功");
+                    }
+                    else
+                        ShowStatus("Close Reader" + (i + 1).ToString() + " 失敗。錯誤代碼:");
+                }
+
+                thread0.Suspend();
+                thread1.Suspend();
+                thread2.Suspend();
+                TagDataProcess.Suspend();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("請確認設備連線狀態");
+            }
+
+            btn_CloseReader.Enabled = false;
+            btn_OpenReader.Enabled = true;
+            btn_AutoReadUltraLight.Enabled = true;
+        }
+
+        private void btn_AutoReadUltraLight_Click(object sender, EventArgs e)
+        {
+          
+            thread0 = new Thread(readNFC);
+            thread0.Start(0);
+
+            thread1 = new Thread(readNFC);
+            thread1.Start(1);
+
+            thread2 = new Thread(readNFC);
+            thread2.Start(2);
+
+
+            TagDataProcess = new Thread(Dataprocess);
+            TagDataProcess.Start();
+
+
+            btn_AutoReadUltraLight.Enabled = false;
+
+            BeginInvoke((Action)delegate
+            {
+                txtmsg.Clear();
+            });
+        }
+
+        private void ShowStatus(String Msg)
+        {
+            txtmsg.Text += Msg + "\r\n";
+        }
+
+        private void readconfig()
+        {
+            string path = Directory.GetCurrentDirectory() + @"\configer.ini";
+            StreamReader sr = new StreamReader(path);
+            string port = sr.ReadToEnd();
+            ports = port.Split(',');
+            sr.Close();
+        }
+
+        private void writedata(string data)
+        {
+            if (data.Length > (Convert.ToInt32(txtUID.Text) + Convert.ToInt32(txtLOT.Text) + Convert.ToInt32(txtUrl.Text)))
+            {
+                string path = Directory.GetCurrentDirectory() + @"\ndef.txt";
+                StreamWriter sw = new StreamWriter(path, false);
+                sw.WriteLine(data);
+                sw.Close();
+            }
+        }
+
+        private void btn_CloseReader_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+
+        }
+
+        private void readNFC(object comNo)
+        {
+            while (true)
+            {
+                try
+                {
+                    //GET UID
+                    int i, j;
+                    byte Sak;
+                    byte[] Uid = new byte[(int)RDINT.OPENCARD_DEF.UID_LEN];
+                    byte[] Atqa = new byte[(int)RDINT.OPENCARD_DEF.ATQA_LEN];
+
+                    strUID = "";
+
+                    //u1AutoFind設定TURN_ON，卡片關閉後必須重新靠卡，才可開啟卡片
+                    M_iResult = (int)RDINT.RDINTnon.OpenCard(devcomport2[(int)comNo], (RDINT.TURN_ON_OFF)RDINT.TURN_ON_OFF.TURN_ON, Uid, Atqa, out Sak);
+
+                    for (i = (int)RDINT.OPENCARD_DEF.UID_LEN - 1; i >= 0; i -= 1)
+                    {
+                        if (Uid[i] == (int)RDINT.OPENCARD_DEF.UID_END)
+                            break;
+                    }
+
+                    j = i;
+
+                    for (i = 0; i < j; i++)
+                    {
+                        strUID += Uid[i].ToString("X2");
+                    }
+
+                    if (strUID.Length == Convert.ToInt32(txtUID.Text))
+                    {
+                        // GET URL
+                        //依指定的區塊(Block)位置，讀取UltraLight卡片內的資料
+                        Data = new byte[(int)RDINT.ULTRA_LIGHT.ULTRA_LIGHT_READ_LEN];
+                        Data2 = new byte[(int)RDINT.ULTRA_LIGHT.ULTRA_LIGHT_READ_LEN];
+                        DataURL1 = new byte[(int)RDINT.ULTRA_LIGHT.ULTRA_LIGHT_READ_LEN];
+                        DataURL2 = new byte[(int)RDINT.ULTRA_LIGHT.ULTRA_LIGHT_READ_LEN];
+
+                        //block 5  ,16 byte
+                        RDINT.RDINTnon.ReadUltraLight(devcomport2[(int)comNo], 6, Data);
+                        RDINT.RDINTnon.ReadUltraLight(devcomport2[(int)comNo], 10, Data2);
+                        RDINT.RDINTnon.ReadUltraLight(devcomport2[(int)comNo], 15, DataURL1);
+                        RDINT.RDINTnon.ReadUltraLight(devcomport2[(int)comNo], 19, DataURL2);
+                    }                
+                }
+                catch (Exception ex) { }
+
+                //  Thread.Sleep(Convert.ToInt32(textBox1.Text));
+                Thread.Sleep(1);
+            }
+        }
+
+        private void Dataprocess()
+        {
+            while (true)
+            {
+                if (Data != null && Data2 != null && DataURL1 != null && DataURL2 != null)
+                {
+                    byte[] newdata1 = new byte[15];
+                    byte[] newdata2 = new byte[5];
+                    byte[] newURL1 = new byte[13];
+
+                    //Array.Copy(a, 2, b, 0, 3); //从a的第二个开始复制到b的第0个，一共复制三个元素
+                    Array.Copy(Data, 1, newdata1, 0, 15);
+                    Array.Copy(Data2, 0, newdata2, 0, 5);
+                    Array.Copy(DataURL1, 3, newURL1, 0, 13);
+
+                    string lotno = Encoding.ASCII.GetString(newdata1) + Encoding.ASCII.GetString(newdata2);
+
+                    if (lotno.Length == Convert.ToInt32(txtLOT.Text))
+                    {
+                        string strurl = Encoding.ASCII.GetString(newURL1);
+
+                        //正規表示url
+                        List<String> rzList = new List<String>();
+                        Match match = Regex.Match(Encoding.ASCII.GetString(DataURL2), @"[a-zA-Z0-9\./_]+");
+
+                        for (int x = 0; match.Success; x++)
+                        {
+                            rzList.Add(match.Groups[0].Value);
+                            match = match.NextMatch();
+                        }
+
+                        for (int x = 0; x < rzList.Count; x++)
+                        {
+                            strurl += rzList[x];
+                        }
+
+                        if (strurl.Length == Convert.ToInt32(txtUrl.Text))
+                        {
+                            string dataout = "";
+
+                            dataout = lotno.Trim('\0') + "\t" + strUID.Trim('\0') + "\t" + strurl.Trim('\0');
+
+                            writedata(dataout);
+
+                            BeginInvoke((Action)delegate
+                            {
+                                txtmsg.Text = strUID;
+                            });
+                        }
+                    }
+                }
+               
+                Thread.Sleep(Convert.ToInt32(textBox1.Text));
+            }
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.notifyIcon1.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+    }
+}
